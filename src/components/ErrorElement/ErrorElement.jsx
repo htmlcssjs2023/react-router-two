ba69400c135aa5cc60a517799b0e8529c9f8680e
mdabdulhalim <htmlcssjs2023@gmail.com>
2023-08-21 07:51:09 +0600

    simple react router

---
+++ src/components/ErrorElement/ErrorElement.jsx
@@ -0,0 +1,13 @@
+import React from 'react';
+import './ErrorElement.css';
+
+const ErrorElement = () => {
+    return (
+        <div>
+           <span>Page is not found ! </span>
+           <p>404 Error is happend.</p>
+        </div>
+    );
+};
+
+export default ErrorElement;
--- src/main.jsx
+++ src/main.jsx
@@ -1,19 +1,28 @@
 import React from 'react'
-import ReactDOM from 'react-dom/client'
-import App from './App.jsx'
+import ReactDOM from 'react-dom/client';
+import './index.css'
 import {
   createBrowserRouter,
   RouterProvider,
 } from "react-router-dom";
 
-import './index.css'
+import ErrorElement from './components/ErrorElement/ErrorElement';
+import About from './components/About/About';
 
 const router = createBrowserRouter([
-  {
-    path: "/",
-    element: <div>Hello world!</div>,
-  },
+    {
+      path:"/",
+      element:<h1>Welcome To React Router World</h1>,
+      errorElement:<ErrorElement></ErrorElement>,
+      
+    },
+    {
+      path:"/about",
+      element:<About />
+    }
 ]);
+
+
 
 
 ReactDOM.createRoot(document.getElementById('root')).render(
